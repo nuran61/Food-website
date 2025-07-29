216e164b3052ad0a5c7336496c237423fe20a992
nuran61 <149605159+nuran61@users.noreply.github.com>
2025-07-29 18:31:37 +0300

    coffee-website

---
+++ public/js/homepage.js
@@ -0,0 +1,30 @@
+$(document).ready(function ($) {
+    "use strict";
+
+
+    jQuery(".menu-toggle").click(function () {
+        jQuery(".main-navigation").toggleClass("toggled");
+    });
+
+    jQuery(".header-menu ul li a").click(function () {
+        jQuery(".main-navigation").removeClass("toggled");
+    });
+
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    var elementFirst = document.querySelector('.site-header');
+    ScrollTrigger.create({
+        trigger: "body",
+        start: "30px top",
+        end: "bottom bottom",
+
+        onEnter : () => myFunction(),
+        onLeaveBack : () => myFunction(),
+    });
+
+    function myFunction(){
+        elementFirst.classlist.toggle('sticky_head');
+
+    }
+})
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Food Website</title>
+    <!-- for icons -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <!-- for swiper slider -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <!-- for custom css -->
+    <link rel="stylesheet" href="{{asset('style/css/homepage.css')}}">
+
+</head>
+<body class="body-fixed">
+<!-- start of header -->
+<header class="site-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2">
+                <div class=" header-logo">
+                    <a href="homepage.blade.php">
+                        <img src="img/logo.png" width="50" height="50" alt="Logo">
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-10">
+                <div class="main-navigation">
+                    <button class="menu-toggle"><span></span><span></span></button>
+                    <nav class="header-menu">
+                        <ul class="menu food-nav-menu">
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#menu">Menu</a></li>
+                            <li><a href="#gallery">Gallery</a></li>
+                            <li><a href="#blog">Blog</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </nav>
+                    <div class="header-right">
+                        <form action="#" class="header-search-form for-des">
+                            <input type="search" class="form-input" placeholder="Search Here...">
+                            <button type="submit">
+                               <i class="uil uil-search"></i>"
+
+                            </button>
+                        </form>
+                        <a href="javascript:void(0)" class="header-btn header-cart">
+                            <i class="uil uil-shopping-bag"></i>
+                            <span class="cart-number">3</span>
+                        </a>
+                        <a href="javascript:void(0)" class="header-btn">
+                            <i class="uil uil-user"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<!-- jquery -->
+<script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
+<!--bootstrop-->
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/popper.min.js') }}"></script>
+
+<!--fontawsome-->
+<script src="{{ asset('js/font-awesome.min.js') }}"></script>
+
+<!--swiper slider-->
+<script src="{{ asset('js/swiper-bundle.min.js') }}"></script>
+
+<!--mixitup--filter-->
+<script src="{{ asset('js/jquery.mixitup.min.js') }}"></script>
+
+<!--fancy box-->
+<script src="{{ asset('js/jquery.fancybox.min.js') }}"></script>
+
+<!--parallax-->
+<script src="{{ asset('js/parallax.min.js') }}"></script>
+
+ <!--gsap--->
+<script src="{{ asset('js/gsap.min.js') }}"></script>
+
+<!--scroll trigger-->
+<script src="{{ asset('js/ScrollTrigger.min.js') }}"></script>
+
+<!--scroll to plug in-->
+<script src="{{ asset('js/ScrollToPlugin.min.js') }}"></script>
+
+<!--rellax-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
+
+<!--smooth scroll-->
+<script src="{{ asset('js/smooth-scroll.js') }}"></script>
+
+<!--custom js-->
+<script src="{{ asset('js/homepage.js') }}"></script>
+
+
+</body>
+</html>
